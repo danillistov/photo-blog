370ad2f3a3b9336c633334a5ec2e78f65da4ed8b
danil <danil>
2023-02-07 21:54:37 +0900

    feat(global): crate navigation structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import PopularPageView from '../views/PopularPageView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'popular',
+      component: PopularPageView,
+    },
+
+    {
+      path: '/new',
+      name: 'new',
+      component: () => import('../views/NewPageView.vue'),
+    },
+
+    {
+      path: 'posts/:id',
+      name: 'post',
+      component: () => import('../views/PostPageView.vue'),
+    },
+
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/',
     },
   ],
 });
