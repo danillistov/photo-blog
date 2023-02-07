14d8eea0a10ebebcf9b75b5cdaf4fe7cf2e6c6b9
danil <danil>
2023-02-07 19:20:13 +0900

    feat(global): project init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import TheNav from '@/components/layout/TheNav.vue';
+</script>
+
+<template>
+  <header>
+    <TheNav />
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/components/layout/TheNav.vue
@@ -0,0 +1,11 @@
+<template>
+  <Disclosure as="nav" class="bg-gray-800">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-between"></div>
+    </div>
+  </Disclosure>
+</template>
+
+<script setup lang="ts">
+import { Disclosure } from '@headlessui/vue';
+</script>
